c2b749fdd46dfbf9ba4c3e4688955d9e50340b04
karabellyj <jozefkarabelly@gmail.com>
2019-05-11 11:06:11 +0200

    correct valid moves generation for Rook

--- src/chess/piece/Rook.java
+++ src/chess/piece/Rook.java
@@ -21,56 +21,52 @@ public class Rook extends Piece {
     // FIXME: remove code duplication
     public static MoveList getMoves(Piece piece, MoveList list) {
         Position pos = piece.getPosition();
+
         int x = pos.getX();
         int y = pos.getY();
-
         while (x >= 0) {
             x--;
-            Position dest = new Position(pos, x, y);
-
+            Position dest = new Position(x, y);
             if (!list.addCapture(new Move(pos, dest))) {
                 break;
             }
-            if (!piece.getBoard().isFree(pos)) {
+            if (!piece.getBoard().isFree(dest)) {
                 break;
             }
         }
-
         x = pos.getX();
-        while (y >= 0) {
-            y--;
-            Position dest = new Position(pos, x, y);
-
-            if (!list.addCapture(new Move(pos, dest))) {
-                break;
-            }
-            if (!piece.getBoard().isFree(pos)) {
-                break;
-            }
-        }
-
         y = pos.getY();
         while (x < piece.getBoard().getWidth()) {
             x++;
-            Position dest = new Position(pos, x, y);
-
+            Position dest = new Position(x, y);
             if (!list.addCapture(new Move(pos, dest))) {
                 break;
             }
-            if (!piece.getBoard().isFree(pos)) {
+            if (!piece.getBoard().isFree(dest)) {
                 break;
             }
         }
-
         x = pos.getX();
-        while (y < piece.getBoard().getHeight()) {
-            y++;
-            Position dest = new Position(pos, x, y);
-
+        y = pos.getY();
+        while (y >= 0) {
+            y--;
+            Position dest = new Position(x, y);
             if (!list.addCapture(new Move(pos, dest))) {
                 break;
             }
-            if (!piece.getBoard().isFree(pos)) {
+            if (!piece.getBoard().isFree(dest)) {
+                break;
+            }
+        }
+        x = pos.getX();
+        y = pos.getY();
+        while (y < piece.getBoard().getHeight()) {
+            y++;
+            Position dest = new Position(x, y);
+            if (!list.addCapture(new Move(pos, dest))) {
+                break;
+            }
+            if (!piece.getBoard().isFree(dest)) {
                 break;
             }
         }
